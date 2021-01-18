690a7305e223244f1836b8123fd6f054fdf6721d
MD Rashid Hussain <64685317+m3rashid@users.noreply.github.com>
2021-01-18 10:19:46 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,162 @@
+from tkinter import *
+
+root = Tk()
+root.title("Calculator")
+
+
+def button_click(number):
+    current = e.get()
+    e.delete(0, END)
+    e.insert(0, str(current) + str(number))
+
+
+def button_clear():
+    e.delete(0, END)
+
+
+def button_add():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "addition"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+
+def button_subtract():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "subtraction"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+
+def button_multiply():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "multiplication"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+
+def button_divide():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "division"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+
+def button_sqrt():
+    first_number = e.get()
+    global f_num
+    f_num = int(first_number)
+    e.delete(0, END)
+    e.insert(0, f_num ** (1 / 2))
+
+
+def button_square():
+    first_number = e.get()
+    global f_num
+    f_num = int(first_number)
+    e.delete(0, END)
+    e.insert(0, (f_num ** 2))
+
+
+def button_reciprocal():
+    first_number = e.get()
+    global f_num
+    f_num = int(first_number)
+    e.delete(0, END)
+    e.insert(0, (1 / f_num))
+
+
+def button_factorial():
+    first_number = e.get()
+    global f_num
+    f_num = int(first_number)
+    e.delete(0, END)
+    factorial = 1
+    while f_num >= 1:
+        factorial *= f_num
+        f_num -= 1
+    e.insert(0, factorial)
+
+
+def button_equal():
+    second_number = e.get()
+    e.delete(0, END)
+
+    if math == "addition":
+        e.insert(0, f_num + int(second_number))
+
+    if math == "subtraction":
+        e.insert(0, f_num - int(second_number))
+
+    if math == "multiplication":
+        e.insert(0, f_num * int(second_number))
+
+    if math == "division":
+        e.insert(0, f_num / int(second_number))
+
+
+e = Entry(root, width=60, borderwidth=5)
+e.grid(row=0, column=0, columnspan=4, padx=10, pady=15)
+
+
+# Number Buttons
+button_1 = Button(root, text="1", padx=40, pady=10, command=lambda: button_click(1))
+button_2 = Button(root, text="2", padx=40, pady=10, command=lambda: button_click(2))
+button_3 = Button(root, text="3", padx=40, pady=10, command=lambda: button_click(3))
+button_4 = Button(root, text="4", padx=40, pady=10, command=lambda: button_click(4))
+button_5 = Button(root, text="5", padx=40, pady=10, command=lambda: button_click(5))
+button_6 = Button(root, text="6", padx=40, pady=10, command=lambda: button_click(6))
+button_7 = Button(root, text="7", padx=40, pady=10, command=lambda: button_click(7))
+button_8 = Button(root, text="8", padx=40, pady=10, command=lambda: button_click(8))
+button_9 = Button(root, text="9", padx=40, pady=10, command=lambda: button_click(9))
+button_0 = Button(root, text="0", padx=40, pady=10, command=lambda: button_click(0))
+
+# Action Buttons
+button_add = Button(root, text="+", padx=39, pady=10, command=button_add)
+button_subtract = Button(root, text="-", padx=40, pady=10, command=button_subtract)
+button_multiply = Button(root, text="*", padx=40, pady=10, command=button_multiply)
+button_divide = Button(root, text="/", padx=40, pady=10, command=button_divide)
+
+button_sqrt = Button(root, text="Sqrt", padx=32, pady=10, command=button_sqrt)
+button_square = Button(root, text="^2", padx=36, pady=10, command=button_square)
+button_reciprocal = Button(root, text="1/n", padx=34, pady=10, command=button_reciprocal)
+button_factorial = Button(root, text="n!", padx=38, pady=10, command=button_factorial)
+
+button_equals = Button(root, text="=", padx=39, pady=10, command=button_equal)
+button_clear = Button(root, text="C", padx=39, pady=10, command=button_clear)
+
+# Putting number button on the screen
+button_1.grid(row=3, column=0)
+button_2.grid(row=3, column=1)
+button_3.grid(row=3, column=2)
+button_4.grid(row=2, column=0)
+button_5.grid(row=2, column=1)
+button_6.grid(row=2, column=2)
+button_7.grid(row=1, column=0)
+button_8.grid(row=1, column=1)
+button_9.grid(row=1, column=2)
+button_0.grid(row=4, column=0)
+
+# Putting action buttons on the screen
+button_add.grid(row=1, column=3)
+button_subtract.grid(row=2, column=3)
+button_multiply.grid(row=3, column=3)
+button_divide.grid(row=4, column=3)
+
+button_sqrt.grid(row=5, column=2)
+button_square.grid(row=4, column=1)
+button_reciprocal.grid(row=5, column=0)
+button_factorial.grid(row=5, column=1)
+
+button_clear.grid(row=4, column=2)
+button_equals.grid(row=5, column=3)
+
+root.mainloop()
